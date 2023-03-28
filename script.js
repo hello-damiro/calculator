f865e7052a84ce639bfcdf13e72c74718450e2d5
damiro <hello.damiro@gmail.com>
2023-03-28 14:11:58 +0800

    solved some bugs onn layout when key pressed

--- script.js
+++ script.js
@@ -1,9 +1,13 @@
-let keys = document.querySelectorAll('.key');
-keys.forEach((key) => {
-    key.addEventListener('click', () => {
-        console.log(key.getAttribute('value'));
-    });
+let lineOneArray = []; // 25 chars
+let lineTwoArray = []; // 14 chars
+let pressedArray = [];
+let equationArray = [];
 
+let lineOneText = document.querySelector('#lcd > h2');
+let lineTwoText = document.querySelector('#lcd > h3');
+let keys = document.querySelectorAll('.key');
+
+keys.forEach((key) => {
     key.addEventListener('mousedown', () => {
         key.classList.add('pressed');
     });
@@ -15,4 +19,12 @@ keys.forEach((key) => {
     key.addEventListener('mouseleave', () => {
         key.classList.remove('pressed');
     });
+
+    key.addEventListener('click', () => {
+        pressedArray.push(key.getAttribute('value'));
+        lineTwoText.innerHTML = lineTwoArray;
+        // console.log(pressedArray);
+    });
 });
+
+function keyActions() {}
