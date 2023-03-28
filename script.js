552f103b58f6b44414b7a3abf37177815a4e149c
damiro <hello.damiro@gmail.com>
2023-03-28 12:42:32 +0800

    added fancy key press animation

--- script.js
+++ script.js
@@ -3,4 +3,16 @@ keys.forEach((key) => {
     key.addEventListener('click', () => {
         console.log(key.getAttribute('value'));
     });
+
+    key.addEventListener('mousedown', () => {
+        key.classList.add('pressed');
+    });
+
+    key.addEventListener('mouseup', () => {
+        key.classList.remove('pressed');
+    });
+
+    key.addEventListener('mouseleave', () => {
+        key.classList.remove('pressed');
+    });
 });
