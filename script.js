1c7463997c7e141d112527b3061035b8db0ac33b
damiro <hello.damiro@gmail.com>
2023-03-29 09:51:26 +0800

    Done with updateNumberPressed and updateEquation

--- script.js
+++ script.js
@@ -16,11 +16,42 @@ let lineTwoText = document.querySelector('#lcd > h3');
 
 export function deleteLastChar() {}
 
-export function updateEquation(operand) {}
+export function updateEquation(operand) {
+    if (pressedKeysArray.length == 0) {
+        equationArray.pop();
+        equationArray.push(operand);
+    } else {
+        equationArray.push(pressedKeysArray.join(''));
+        equationArray.push(operand);
+    }
+
+    updateLineOne();
+
+    // Reset Display Line two
+    lineTwoText.textContent = '';
+    pressedKeysArray = [];
+}
+
+export function performCalculation() {
+    console.log('performing calculation ...');
+}
 
 export function updateNumberPressed(number) {
     pressedKeysArray.push(number);
+    updateLineTwo();
+}
 
+export function clearAll() {
+    pressedKeysArray = [];
+    equationArray = [];
+    lineOneDisplay = [];
+    lineTwoDisplay = [];
+    lineOneText.textContent = '';
+    lineTwoText.textContent = '0';
+}
+
+function updateLineTwo() {
+    // Limit LCD display to 14 chars
     const lcdLineTwoLength = 14;
     if (pressedKeysArray.length >= lcdLineTwoLength) {
         lineTwoDisplay = pressedKeysArray.slice(
@@ -31,16 +62,23 @@ export function updateNumberPressed(number) {
         lineTwoDisplay = pressedKeysArray.slice(0, lcdLineTwoLength);
     }
 
+    // Display number pressed
     lineTwoText.textContent = lineTwoDisplay.join('');
 }
 
-export function clearAll() {
-    pressedKeysArray = [];
-    equationArray = [];
-    lineOneText.textContent = '';
-    lineTwoText.textContent = '0';
-}
+function updateLineOne() {
+    // Limit LCD display to 25 chars
+    const lcdLineOneLength = 25;
+    const equationString = equationArray.join('');
+    if (equationString.length >= lcdLineOneLength) {
+        lineOneDisplay = equationString.slice(
+            equationString.length - lcdLineOneLength,
+            equationString.length
+        );
+    } else {
+        lineOneDisplay = equationString.slice(0, lcdLineOneLength);
+    }
 
-export function performCalculation() {
-    console.log('performing calculation ...');
+    // Display number pressed
+    lineOneText.textContent = lineOneDisplay;
 }
