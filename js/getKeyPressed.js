da6ae28f34b0287632f9cc41b10940d51d7f6be2
damiro <hello.damiro@gmail.com>
2023-03-29 09:03:31 +0800

    lets try again after a nice rest

---
+++ js/getKeyPressed.js
@@ -0,0 +1,54 @@
+import {
+    numbersAllowed,
+    operandsAllowed,
+    updateNumberPressed,
+    updateEquation,
+    performCalculation,
+    deleteLastChar,
+    clearAll,
+} from '/script.js';
+
+export function getKeyPressed() {
+    let keys = document.querySelectorAll('.key');
+    keys.forEach((key) => {
+        key.addEventListener('mousedown', () => {
+            key.classList.add('pressed');
+        });
+        key.addEventListener('mouseup', () => {
+            key.classList.remove('pressed');
+        });
+        key.addEventListener('mouseleave', () => {
+            key.classList.remove('pressed');
+        });
+    });
+
+    // GUI
+    let numbers = document.querySelectorAll('.number');
+    numbers.forEach((number) => {
+        number.addEventListener('click', () => {
+            updateNumberPressed(number.getAttribute('value'));
+        });
+    });
+
+    let operands = document.querySelectorAll('.operand');
+    operands.forEach((operand) => {
+        operand.addEventListener('click', () => {
+            updateEquation(operand.getAttribute('value'));
+        });
+    });
+
+    document.querySelector('.equal').addEventListener('click', performCalculation);
+    document.querySelector('.c').addEventListener('click', deleteLastChar);
+    document.querySelector('.ac').addEventListener('click', clearAll);
+
+    // KEYBOARD
+    document.addEventListener('keydown', function (event) {
+        // TODO: on key press animate GUI
+        const key = event.key; // "a", "1", "Shift", etc.
+        if (numbersAllowed.includes(key)) updateNumberPressed(key);
+        else if (operandsAllowed.includes(key)) updateEquation(key);
+        else if ((key = '=')) performCalculation();
+        else if ((key = 'Backspace')) deleteLastChar();
+        else if ((key = 'Delete')) clearAll();
+    });
+}
--- script.js
+++ script.js
@@ -1,100 +1,24 @@
-let lineOneDisplay = []; // 25 chars
-let lineTwoDisplay = []; // 12 chars, 14 incl 1 and period
-let pressedKeysArray = [];
-let equationArray = [];
+import { getKeyPressed } from './js/getKeyPressed.js';
 
-let numbersAllowed = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '.'];
-let operandsAllowed = ['*', '/', '+', '-']; // MDAS
+export let numbersAllowed = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '.'];
+export let operandsAllowed = ['*', '/', '+', '-']; // MDAS
+
+export let lineOneDisplay = []; // 25 chars
+export let lineTwoDisplay = []; // 12 chars, 14 incl 1 and period
+
+export let pressedKeysArray = [];
+export let equationArray = [];
+
+getKeyPressed();
 
 let lineOneText = document.querySelector('#lcd > h2');
 let lineTwoText = document.querySelector('#lcd > h3');
 
-getKeyPressed();
+export function deleteLastChar() {}
 
-function getKeyPressed() {
-    let keys = document.querySelectorAll('.key');
-    keys.forEach((key) => {
-        key.addEventListener('mousedown', () => {
-            key.classList.add('pressed');
-        });
-        key.addEventListener('mouseup', () => {
-            key.classList.remove('pressed');
-        });
-        key.addEventListener('mouseleave', () => {
-            key.classList.remove('pressed');
-        });
-    });
+export function updateEquation(operand) {}
 
-    // GUI
-    let numbers = document.querySelectorAll('.number');
-    numbers.forEach((number) => {
-        number.addEventListener('click', () => {
-            updateNumberPressed(number.getAttribute('value'));
-        });
-    });
-
-    let operands = document.querySelectorAll('.operand');
-    operands.forEach((operand) => {
-        operand.addEventListener('click', () => {
-            updateEquation(operand.getAttribute('value'));
-        });
-    });
-
-    document.querySelector('.equal').addEventListener('click', performCalculation);
-    document.querySelector('.c').addEventListener('click', deleteLastChar);
-    document.querySelector('.ac').addEventListener('click', clearAll);
-
-    // KEYBOARD
-    document.addEventListener('keydown', function (event) {
-        // TODO: on key press animate GUI
-        const key = event.key; // "a", "1", "Shift", etc.
-        if (numbersAllowed.includes(key)) updateNumberPressed(key);
-        else if (operandsAllowed.includes(key)) updateEquation(key);
-        else if ((key = '=')) performCalculation();
-        else if ((key = 'Backspace')) deleteLastChar();
-        else if ((key = 'Delete')) clearAll();
-    });
-}
-
-function deleteLastChar() {
-    equationArray.push(pressedKeysArray.join(''));
-    lineTwoText.textContent = equationArray[equationArray.length - 1];
-}
-
-function clearAll() {
-    pressedKeysArray = [];
-    equationArray = [];
-    lineOneText.textContent = '';
-    lineTwoText.textContent = '0';
-}
-
-function performCalculation() {
-    console.log('performing calculation ...');
-}
-
-function updateEquation(operand) {
-    equationArray.push(pressedKeysArray.join(''));
-    equationArray.push(operand);
-    pressedKeysArray = [];
-    lineTwoText.textContent = '';
-
-    const lcdLineOneLength = 25;
-    const equationString = equationArray.join('');
-
-    if (equationString.length >= lcdLineOneLength) {
-        lineOneDisplay = equationString.slice(
-            equationString.length - lcdLineOneLength,
-            equationString.length
-        );
-    } else {
-        lineOneDisplay = equationString.slice(0, lcdLineOneLength);
-    }
-
-    lineOneText.textContent = lineOneDisplay;
-    console.log('equation: ' + equationArray);
-}
-
-function updateNumberPressed(number) {
+export function updateNumberPressed(number) {
     pressedKeysArray.push(number);
 
     const lcdLineTwoLength = 14;
@@ -106,5 +30,17 @@ function updateNumberPressed(number) {
     } else {
         lineTwoDisplay = pressedKeysArray.slice(0, lcdLineTwoLength);
     }
+
     lineTwoText.textContent = lineTwoDisplay.join('');
+}
+
+export function clearAll() {
+    pressedKeysArray = [];
+    equationArray = [];
+    lineOneText.textContent = '';
+    lineTwoText.textContent = '0';
+}
+
+export function performCalculation() {
+    console.log('performing calculation ...');
 }
